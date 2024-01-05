2c7c94c0c79384d19654b525fbd0f0fd8cf1340f
Miarotianastephan <miarotianastephan01@gmail.com>
2024-01-05 16:08:31 +0300

    Initial commit

---
+++ src/test/java/com/testdev/TestdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
